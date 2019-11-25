2d8dfabd3ab7f16adea5fd7b332d4d058b702a00
Ruhua Wang <Ruhua.Wang@morningstar.com>
2019-11-25 13:35:39 -0600

    Initial Commit

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const open = require('open');
+
+const argv = require('yargs')
+  .version('sss')
+  .usage('Usage: udeploy-cli snapshot|deploy [options]')
+  .command(['snapshot'], 'command for snapshot ', require('./lib/snapshot'))
+  .command(['deploy'], 'command for snapshot ', require('./lib/deploy'))
+  .demandCommand(1, 'You need at least one command before moving on')
+  .help('h')
+  .alias('h', 'help')
+  .argv;
