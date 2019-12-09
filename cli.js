7fc03dfaa3fb4abec5d02c778457b2085c04645e
Ruhua Wang <Ruhua.Wang@morningstar.com>
2019-12-09 12:22:24 -0600

    add exports add promise for add status function

--- cli.js
+++ cli.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const open = require('open');
-
 const argv = require('yargs')
   .version('sss')
-  .usage('Usage: udeploy-cli snapshot|deploy [options]')
-  .command(['snapshot'], 'command for snapshot ', require('./lib/snapshot'))
+  .usage('Usage: $0 snapshot|deploy|app [options]')
+  .command(['snapshot'], 'command for snapshot ',{}, require('./lib/snapshot'))
+  .command(['deploy'], 'command for deploy', {},require('./lib/deploy'))
+  .command(['app'], 'command for deploy', {},require('./lib/application'))
   .demandCommand(1, 'You need at least one command before moving on')
   .help('h')
   .alias('h', 'help')
